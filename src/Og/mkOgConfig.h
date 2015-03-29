3ac5001353fc93b0056f2537a4ab3aa715280023
NovemberMonk <hugo.amiard@laposte.net>
2015-03-30 01:10:05 +0200

    Fix Build clang gcc

--- src/Og/mkOgConfig.h
+++ src/Og/mkOgConfig.h
@@ -9,10 +9,29 @@
 #include <Sound/mkSoundConfig.h>
 #include <Ui/mkUiConfig.h>
 
-#if defined OG_EXPORT
-#define MK_OG_EXPORT __declspec(dllexport)
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef OG_EXPORT
+    #ifdef __GNUC__
+      #define MK_OG_EXPORT __attribute__ ((dllexport))
+    #else
+      #define MK_OG_EXPORT __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #else
+    #ifdef __GNUC__
+      #define MK_OG_EXPORT __attribute__ ((dllimport))
+    #else
+      #define MK_OG_EXPORT __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #endif
+  #define DLL_LOCAL
 #else
-#define MK_OG_EXPORT __declspec(dllimport) 
+  #if __GNUC__ >= 4
+    #define MK_OG_EXPORT __attribute__ ((visibility ("default")))
+    #define DLL_LOCAL __attribute__ ((visibility ("hidden")))
+  #else
+    #define MK_OG_EXPORT
+    #define DLL_LOCAL
+  #endif
 #endif
 
 #endif // MK_OGCONFIG_H_INCLUDED
